e1b77270f819d8e2feffbd19dd7e4eeff4b6cb54
farisamirmudin <farisamirmudin@gmail.com>
2022-11-24 22:21:47 +0800

    change bg

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,14 +11,14 @@ interface Props {
 const Home = ({ collections }: Props) => {
   return (
     <div className="min-h-screen  text-white flex flex-col">
-      <p className='text-4xl text-center py-8 bg-gray-200 text-black'>NFT Market Place</p>
+      <p className='text-4xl text-center py-8  text-black'>NFT Market Place</p>
       <main className='flex flex-col items-center justify-center bg-gradient-to-br from-[#7D77FF] to-[#FF9482] flex-1 gap-y-4'>
         <div>
           {collections && collections.map(collection => {
             return (
               <Link href={`/nft/${collection.slug.current}`} key={collection._id}>
                 <div className='transition-all duration-200 hover:scale-105 text-center'>
-                  <div className="relative bg-gray-200 p-2 rounded-xl">
+                  <div className="relative bg-white p-2 rounded-xl">
                     <Image className='h-72 w-72 object-cover rounded-lg cursor-pointer' src={urlFor(collection.mainImage).url()} alt="" width={200} height={200} priority />
                   </div>
                   <p className='text-3xl mt-4'>{collection.title}</p>
